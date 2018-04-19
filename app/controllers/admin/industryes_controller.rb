71f98f89058b3de74203e7c05a8b5652d8ec2967
jamst <jamstjamst@163.com>
2018-04-19 22:06:55 +0800

    indusrtys

--- app/controllers/admin/industryes_controller.rb
+++ app/controllers/admin/industryes_controller.rb
@@ -1,16 +1,28 @@
 class Admin::IndustryesController < Admin::BaseController
 	def index
 		if params[:category_id]
-			@industryes = Industrye.where(category_id:)
+			@industryes = Industrye.where(category_id:1)
 		elsif
 			@industryes = Industrye.all
 		end
 	end
 
+	def new
+      @industrye = Industrye.new
+	end
 
+
+	def  create
+		@industrye = Industrye.new(industrye_params)
+		@industrye.save
+	end
+
+	def show
+
+	end
 
 	def industrye_params
-		params.require(:industrye).permit(:category_id,:industrye,:parent_id)
+		params.require(:industrye).permit(:category_id,:industry,:parent_id)
 
 	end
 
